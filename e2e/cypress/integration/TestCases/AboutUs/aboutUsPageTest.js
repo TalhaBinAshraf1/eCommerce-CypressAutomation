ced6f07305497a184f70635abc91074ed62e2657
Talha Bin Ashraf <35797610+TalhaBinAshraf1@users.noreply.github.com>
2022-02-12 02:18:57 +0600

    Update aboutUsPageTest.js

--- e2e/cypress/integration/TestCases/AboutUs/aboutUsPageTest.js
+++ e2e/cypress/integration/TestCases/AboutUs/aboutUsPageTest.js
@@ -1,7 +1,7 @@
 describe("base url Visit ", () => {
 
   it("Validating Navigation to Official Website -> Shipping Page", () => {
-    let baseUrl="https://www.nonstopdogwear.com/en/for-dogs/" ;
+  
     cy.visit(baseUrl);
     cy.wait(4000);
     cy.url().should("equal", baseUrl );
