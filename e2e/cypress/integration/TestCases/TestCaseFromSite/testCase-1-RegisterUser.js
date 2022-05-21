ef9ddd6c58db7406d984ad185b25b0836b95b264
unknown <talhabinashraf@gmail.com>
2022-05-22 03:24:04 +0600

    tc 1 done

--- e2e/cypress/integration/TestCases/TestCaseFromSite/testCase-1-RegisterUser.js
+++ e2e/cypress/integration/TestCases/TestCaseFromSite/testCase-1-RegisterUser.js
@@ -1,6 +1,22 @@
 /// <reference types="cypress" />
 
-import tc1rup from '../../../PageObject/testCase-1-RegisterUserPage';
+import tc1rup, { getAccountCreatedText } from '../../../PageObject/testCase-1-RegisterUserPage';
+
+import chaiColors from 'chai-colors' 
+chai.use(chaiColors)
+
+
+const positiveMessage = "Inputted Successfully" ; 
+const elementVisible = "Is Visible";
+const firstName = "Alex" ;
+const lastName = "Smakov" ;
+const companyName = "TestCompany";
+const address =  "145-W,Troy,Newyork,USA";
+const address2 ="145-W,Newyork ";
+const state = "NewYork";
+const city = "Troy";
+const zipCode =  "1256";
+const mobileNumber = "+01235525" ; 
 
 
 describe("Test Case 1: Register User", () =>{
@@ -20,7 +36,7 @@ describe("Test Case 1: Register User", () =>{
        const signupLogin = tc1rup.getSignupLoginButton();
         if (signupLogin.should("be.visible")) {
           signupLogin.click(); 
-          cy.log(signupLogin + "Is Visible ");
+          cy.log(signupLogin + elementVisible);
         } else {
             cy.visit(Cypress.env("url"));
         }
@@ -32,7 +48,7 @@ describe("Test Case 1: Register User", () =>{
       if (newUserSignup.should("be.visible")) {
           newUserSignup.should("include.text","New User Signup!")
           .and("have.length","1");
-          cy.log(newUserSignup + "Is Visible ");
+          cy.log(newUserSignup + elementVisible);
       } else {
           cy.visit(Cypress.env("url"));
       }
@@ -45,27 +61,24 @@ describe("Test Case 1: Register User", () =>{
     if(nameInputField.should("be.visible"))
     {
       nameInputField.focus();
-      //nameInputField.contains("Name");
-      nameInputField.type("test01289");
-      cy.log(" Name Inputted Successfully")
+      nameInputField.type(Cypress.env("name"));
+      cy.log( nameInputField + positiveMessage)
     }else{
       cy.visit(Cypress.env("url"));
     }
+
     const emailInputField = tc1rup.getEmailInputField();
     if(emailInputField.should("be.visible"))
     {
       emailInputField.focus();
-      //cy.get(".signup-form > form[method='post'] > input[name='email']").invoke('val').should('contain', 'Email Address')
-      //emailInputField.invoke('attr', 'placeholder').should('contain', 'Email Address')
-      emailInputField.type("alexsmkovtkvtzb489799@sgisfg.com");
-      cy.log(" Email Inputted Successfully ");
+      emailInputField.type(Cypress.env("email"));
+      cy.log( emailInputField + positiveMessage );
     }else{
       cy.visit(Cypress.env("url"));
     }
 
   });
   
-
   // it("should login using the custom commands", () => {
   //   cy.visit("https://admin-demo.nopcommerce.com/");
   //   cy.login("admin@yourstore.com", "admin");
@@ -103,53 +116,338 @@ it("TC - 8. Verify that 'ENTER ACCOUNT INFORMATION' is visible ", () => {
 
 it(" TC - 9. Fill details: Title, Name, Email, Password, Date of birth ", () => {
 
+    const mrRedioButton = tc1rup.getMrRedioButton();
+    if (mrRedioButton.should("be.visible")) {
+      mrRedioButton.click();
+      cy.log(" ✔✔✔ Button Is Visible and Clickable ✔✔✔");
+    } else {
+      cy.log(" ❌❌❌ Button Is not Visible ❌❌❌");
+    }
+
+    
+    const mrsRedioButton = tc1rup.getMrsRedioButton();
+    if (mrsRedioButton.should("be.visible")) {
+      mrsRedioButton.click();
+      cy.log(" ✔✔✔ Button Is Visible and Clickable ✔✔✔");
+    } else {
+      cy.visit(Cypress.env("url"));
+      cy.log(" ❌❌❌ Button Is not Visible ❌❌❌");
+    }
+
+    //Mr redio Button is clicked 
+    tc1rup.getMrRedioButton().click();
+    cy.wait(2000);
+
+    //Password Inputted 
+    const passwordInputField = tc1rup.getPasswordinputField();
+    if (passwordInputField.should("be.visible")) {
+      passwordInputField.type(Cypress.env("password"));
+      cy.log(" ✔✔✔ Input Field Is Visible and Inputted SucessFully ✔✔✔");
+    } else {
+      cy.visit(Cypress.env("url"));
+      cy.log(" ❌❌❌ Input Field Is not Visible ❌❌❌");
+    }
+
+
+// Date of birth Dropdown 
+
+//Day Dropdown 
+
+cy.dayDropdown("1","1");
+cy.dayDropdown("2","2");
+cy.dayDropdown("3","3");
+cy.dayDropdown("4","4");
+cy.dayDropdown("5","5");
+cy.dayDropdown("6","6");
+cy.dayDropdown("7","7");
+cy.dayDropdown("8","8");
+cy.dayDropdown("9","9");
+cy.dayDropdown("10","10");
+cy.dayDropdown("11","11");
+cy.dayDropdown("12","12");
+cy.dayDropdown("13","13");
+cy.dayDropdown("14","14");
+cy.dayDropdown("15","15");
+cy.dayDropdown("16","16");
+cy.dayDropdown("17","17");
+
+
+//MonthDropdown 
+
+cy.monthDropdown("January","1");
+cy.monthDropdown("February","2");
+cy.monthDropdown("March","3");
+cy.monthDropdown("April","4");
+cy.monthDropdown("May","5");
+cy.monthDropdown("June","6");
+cy.monthDropdown("July","7");
+cy.monthDropdown("August","8");
+cy.monthDropdown("September","9");
+cy.monthDropdown("October","10");
+cy.monthDropdown("November","11");
+cy.monthDropdown("December","12");
+
+//Year Dropdown 
+
+cy.yearDropdown("2021","2021");
+cy.yearDropdown("2020","2020");
+cy.yearDropdown("2019","2019");
+cy.yearDropdown("2018","2018");
+cy.yearDropdown("2017","2017");
+cy.yearDropdown("2016","2016");
+cy.yearDropdown("2015","2015");
+cy.yearDropdown("2014","2014");
+cy.yearDropdown("1996","1996");
+cy.yearDropdown("1995","1995");
+cy.yearDropdown("1994","1994");
+cy.yearDropdown("1993","1993");
+cy.yearDropdown("1992","1992");
+
+//Select the Date of Birth 
+cy.dayDropdown("13","13");
+cy.monthDropdown("May","5");
+cy.yearDropdown("1994","1994");
+
 
 });
 
 it("TC - 10. Select checkbox 'Sign up for our newsletter!'", () => {
 
+  const signUpForOurNewsLetterscheckbox = tc1rup.getSignUpForOurNewsLetterCheckbox();
+  if (signUpForOurNewsLetterscheckbox
+    .should("be.visible")
+    .should("not.be.checked")
+    .should("have.text","Sign up for our newsletter!"))
+     {
+    signUpForOurNewsLetterscheckbox.click();
+    cy.log(signUpForOurNewsLetterscheckbox + "Button Is Visible & Checked");
+  } else {
+    cy.log("Button Not Available!");
+  }
 
 });
 
 it(" TC - 11. Select checkbox 'Receive special offers from our partners!'", () => {
 
+  const recieveSpecialOffersFromOurPartnersCheckbox = tc1rup.getRecivespecialOffersFromOurPartnersCheckbox();
+  if (recieveSpecialOffersFromOurPartnersCheckbox
+    .should("be.visible")
+    .should("not.be.checked")
+    .should("have.text","Receive special offers from our partners!")) {
+    recieveSpecialOffersFromOurPartnersCheckbox.click();
+    cy.log(recieveSpecialOffersFromOurPartnersCheckbox + "Button Is Visible & Checked");
+  } else {
+    cy.log("Button Not Available!");
+  }
 
 });
 
 it(" TC - 12. Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number ", () => {
 
+  //First Name 
+  const firstNameInputField = tc1rup.getFirstNameInputField();
+  if(firstNameInputField.should("be.visible"))
+  {
+    firstNameInputField.focus();
+    firstNameInputField.type(firstName);
+    cy.log( firstNameInputField + positiveMessage );
+  }else{
+    cy.visit(Cypress.env("url"));
+  }
 
+    //Last Name 
+    const lastNameInputField = tc1rup.getLastNameInputField();
+    if(lastNameInputField.should("be.visible"))
+    {
+      lastNameInputField.focus();
+      lastNameInputField.type(lastName);
+      cy.log( lastNameInputField + positiveMessage );
+    }else{
+      cy.visit(Cypress.env("url"));
+    }
+
+
+    //Company Name 
+    const companyNameInputField = tc1rup.getCompanyNameInputField();
+    if(companyNameInputField.should("be.visible"))
+    {
+      companyNameInputField.focus();
+      companyNameInputField.type(companyName);
+      cy.log( companyNameInputField + positiveMessage );
+    }else{
+      cy.visit(Cypress.env("url"));
+    }
+
+ 
+    //Address Name 
+    const addressInputField = tc1rup.getAdressInputField();
+    if(addressInputField.should("be.visible"))
+    {
+      addressInputField.focus();
+      addressInputField.type(address);
+      cy.log( addressInputField + positiveMessage );
+    }else{
+      cy.visit(Cypress.env("url"));
+    } 
+    
+    //Address2 2 Name
+
+    const address2InputField = tc1rup.getAdressInputField();
+    if(address2InputField.should("be.visible"))
+    {
+      address2InputField.focus();
+      address2InputField.type(address2);
+      cy.log( address2InputField + positiveMessage );
+    }else{
+      cy.visit(Cypress.env("url"));
+    }  
+    
+    //Country 
+    cy.countryDropdown("India","India");
+    cy.countryDropdown("United States","United States");
+    cy.countryDropdown("Canada","Canada");
+    cy.countryDropdown("Australia","Australia");
+    cy.countryDropdown("New Zealand","New Zealand");
+    cy.countryDropdown("Singapore","Singapore");
+    cy.countryDropdown("United States","United States");
+
+    //State 
+
+    const stateInputField = tc1rup.getStateInputField();
+    if(stateInputField.should("be.visible"))
+    {
+      stateInputField.focus();
+      stateInputField.type(state);
+      cy.log( stateInputField + positiveMessage );
+    }else{
+      cy.visit(Cypress.env("url"));
+    }  
+
+    //City
+
+    const cityInputField = tc1rup.getCityInputField();
+    if(cityInputField.should("be.visible"))
+    {
+      cityInputField.focus();
+      cityInputField.type(city);
+      cy.log( cityInputField + positiveMessage );
+    }else{
+      cy.visit(Cypress.env("url"));
+    }  
+
+    //Zipcode
+
+    const zipCodeInputField = tc1rup.getZipcodeInputField();
+    if(zipCodeInputField.should("be.visible"))
+    {
+      zipCodeInputField.focus();
+      zipCodeInputField.type(zipCode);
+      cy.log( zipCodeInputField + positiveMessage );
+    }else{
+      cy.visit(Cypress.env("url"));
+    }    
+    
+     //Mobile Number 
+
+    const mobileNumberInputField = tc1rup.getMobileNumberInputField();
+    if(mobileNumberInputField.should("be.visible"))
+    {
+      mobileNumberInputField.focus();
+      mobileNumberInputField.type(mobileNumber);
+      cy.log( mobileNumberInputField + positiveMessage );
+    }else{
+      cy.visit(Cypress.env("url"));
+    } 
 });
 
 it(" TC - 13. Click 'Create Account button' ", () => {
+
+    const  createAccountButton = tc1rup.getCreateAccountButton();
+  if( createAccountButton.should("be.visible").should("have.text","Create Account").should("have.css", "background-color",).and ("be.colored" ,"#FE980F") )
+ 
+  {
+    tc1rup.getCreateAccountButton().click();
+    cy.log("Button is Displayed and Clickable ")
+  }else{
+    cy.visit(Cypress.env("url"));
+  }
 
 
 });
 
 it("  TC - 14. Verify that 'ACCOUNT CREATED!' is visible ", () => {
 
+  const  accountCreatedText = tc1rup.getAccountCreatedText();
+
+  if( accountCreatedText
+    .should("be.visible")
+    .should("have.text","Account Created!"))
+ 
+   {
+     tc1rup.getAccountCreatedText().click()
+     cy.log("Text is Displayed   ")
+   }else{
+     cy.visit(Cypress.env("url"));
+   }
 
 });
 
 it(" TC - 15. Click 'Continue' button ", () => {
 
-
-});
-
-it(" TC - 16. Verify that 'Logged in as username' is visible ", () => {
-
-
-});
-
-it("  TC - 17. Click 'Delete Account' button ", () => {
-
-
-});
-
-it(" TC - 18. Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button ", () => {
+  const  continueButton = tc1rup.getContinueButton();
+   if( continueButton.should("be.visible")
+   .should("have.text","Continue")
+   .should("have.css", "background-color",)
+   .and ("be.colored" ,"#FE980F") )
+ 
+   {
+     tc1rup.getContinueButton().click();
+     cy.log("Button is Displayed and Clickable ")
+   }else{
+     cy.visit(Cypress.env("url"));
+   }
 
 
 });
+
+// it(" TC - 16. Verify that 'Logged in as username' is visible ", () => {
+
+//   cy.wait(2000);
+//   cy.reload();
+//   const  loggedInasUserNameText = tc1rup.getLoggedInAsUsernameText();
+
+//   if( loggedInasUserNameText
+//     .should("be.visible")
+//     .should("have.text"," Logged in as " + Cypress.env("name")))
+ 
+//    {
+//      cy.log("Text is Displayed   ")
+//    }else{
+//      cy.visit(Cypress.env("url"));
+//    }
+
+
+// });
+
+// it("  TC - 17. Click 'Delete Account' button ", () => {
+
+//   const  deleteButton = tc1rup.getDeleteButton();
+//   if( deleteButton.should("be.visible")
+//   .should("have.text","Create Account")
+//   .should("have.css", "background-color",)
+//   .and ("be.colored" ,"#FE980F") )
+
+//   {
+//     tc1rup.getDeleteButton().click();
+//     cy.log("Button is Displayed and Clickable ")
+//   }else{
+//     cy.visit(Cypress.env("url"));
+//   }
+
+
+
+// });
+
 
     
  });
